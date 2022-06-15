be75989c64767e374a45f2597fa4e8ee607c9979
Ashwini <58700518+Ashwini-Sriram@users.noreply.github.com>
2022-06-15 12:10:05 +0530

    Delete wiki directory

--- wiki/encyclopedia/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class search_form(forms.Form):
-    search=forms.CharField(max_length=64,widget=forms.TextInput(attrs={'class':'search','placeholder':'Search Encyclopedia'}))
-
-class create_form(forms.Form):
-    title = forms.CharField(max_length=64,widget=forms.TextInput(attrs={'placeholder':'Enter Title'}))
-    text = forms.CharField(max_length=400000,widget=forms.Textarea(attrs={'placeholder':'Enter text'})) 
-
